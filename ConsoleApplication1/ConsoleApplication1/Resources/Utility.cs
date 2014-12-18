9d7952623bfd929b26e6cc11e03c8e4ce9114c77
vijayjha <mailtovijayjha@gmail.com>
2014-12-18 17:29:51 +0530

    Added file to newly created folder

---
+++ ConsoleApplication1/ConsoleApplication1/Resources/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Resources
+{
+  public  class Utility
+    {
+      public static void Show()
+      {
+          Console.WriteLine("Testing Folder Addition");
+      }
+    }
+}
