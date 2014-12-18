d7fba2e04ed48329fc3c96246d3411a879b29236
vijayjha <mailtovijayjha@gmail.com>
2014-12-18 17:10:25 +0530

    Commit for new check-in

---
+++ ConsoleApp/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean? con1 = null;
+            //bool? con2 = false;
+            if(con1.HasValue && con1.Value)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            if(con1.HasValue  & con1.Value)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadLine();
+        }
+    }
+}
